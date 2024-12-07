2c5fdeb1eab6b10702e23f781d945944025c88e7
al-amin90 <ijesun30@gmail.com>
2024-12-07 18:38:06 +0600

    first commit

---
+++ dist/app/app.js
@@ -0,0 +1,30 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+// parser
+app.use(express_1.default.json());
+app.use(express_1.default.text());
+//router
+const userRouter = express_1.default.Router();
+userRouter.get('/api/v1');
+//middleware
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
+// params
+app.get('/', logger, (req, res) => {
+    // console.log(req.query);         
+    res.send('Hello wdd d orld');
+});
+app.post('/', logger, (req, res) => {
+    console.log(req);
+    res.json({
+        message: "Successfully Received data "
+    });
+});
+exports.default = app;
---
+++ src/app/app.ts
@@ -0,0 +1,33 @@
+import express, { NextFunction, Request, Response } from 'express'
+const app = express()
+
+// parser
+app.use(express.json())
+app.use(express.text())
+
+//router
+const userRouter = express.Router()
+
+userRouter.get('/api/v1')
+
+//middleware
+const logger = (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.url, req.method, req.hostname);
+
+    next()
+}
+
+// params
+app.get('/', logger, (req: Request, res: Response) => {
+    // console.log(req.query);         
+    res.send('Hello wdd d orld')
+})
+
+app.post('/', logger, (req: Request, res: Response) => {
+    console.log(req);
+    res.json({
+        message: "Successfully Received data "
+    })
+})
+
+export default app;
