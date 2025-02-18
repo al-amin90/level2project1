1383843fe56b3aab571af3f7bd3292ae616b9c53
al-amin90 <ijesun30@gmail.com>
2025-02-18 21:14:07 +0600

    middleware added

--- dist/app/app.js
+++ dist/app/app.js
@@ -5,8 +5,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello ppy');
+//parsers
+app.use(express_1.default.json());
+app.use(express_1.default.text());
+//middleware
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
+app.get('/', logger, (req, res) => {
+    console.log(req.query.email);
+    res.send('this is the real path');
+});
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send({
+        message: "successfully receive data"
+    });
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,27 @@
-import express, { Request, Response } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 const app = express()
-const port = 3000
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello ppy')
+//parsers
+app.use(express.json())
+app.use(express.text())
+
+//middleware
+const logger = (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.url, req.method, req.hostname);
+    next()
+}
+
+app.get('/', logger, (req: Request, res: Response) => {
+    console.log(req.query.email);
+    res.send('this is the real path')
+})
+
+
+app.post('/', (req: Request, res: Response) => {
+    console.log(req.body);
+    res.send({
+        message: "successfully receive data"
+    })
 })
 
 export default app;
