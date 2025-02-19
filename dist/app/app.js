104d543ac6dd2cfaba366a705f14cb9314ac3c03
al-amin90 <ijesun30@gmail.com>
2025-02-19 11:54:08 +0600

    error handeing && router added

--- dist/app/app.js
+++ dist/app/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -10,17 +19,61 @@ app.use(express_1.default.json());
 app.use(express_1.default.text());
 //middleware
 const logger = (req, res, next) => {
-    console.log(req.url, req.method, req.hostname);
+    console.log(req.url, req.method);
     next();
 };
-app.get('/', logger, (req, res) => {
-    console.log(req.query.email);
-    res.send('this is the real path');
+// creating diffarent route useing express.Router 
+const userRouter = express_1.default.Router();
+const coursesRouter = express_1.default.Router();
+app.use('/api/v1', userRouter);
+app.use('/api/v1/courses', coursesRouter);
+userRouter.get('/users', (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: "user is created successfully",
+        data: user
+    });
 });
+coursesRouter.post('/create-course', (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: "user is created successfully",
+        data: course
+    });
+});
+// the error
+app.get('/', logger, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        res.send(something);
+    }
+    catch (error) {
+        next(error);
+    }
+}));
 app.post('/', (req, res) => {
     console.log(req.body);
     res.send({
         message: "successfully receive data"
     });
+});
+// wrong route error show
+app.all("*", (req, res) => {
+    res.status(400).json({
+        success: false,
+        message: "Route is not found"
+    });
+});
+//global error
+app.use((error, req, res, next) => {
+    if (error) {
+        res.status(400).json({
+            success: false,
+            message: "Something went wrong"
+        });
+    }
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,3 +1,4 @@
+import { error } from 'console'
 import express, { NextFunction, Request, Response } from 'express'
 const app = express()
 
@@ -7,13 +8,50 @@ app.use(express.text())
 
 //middleware
 const logger = (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.url, req.method, req.hostname);
+    console.log(req.url, req.method);
     next()
 }
 
-app.get('/', logger, (req: Request, res: Response) => {
-    console.log(req.query.email);
-    res.send('this is the real path')
+// creating diffarent route useing express.Router 
+const userRouter = express.Router();
+const coursesRouter = express.Router();
+
+app.use('/api/v1', userRouter)
+app.use('/api/v1/courses', coursesRouter)
+
+userRouter.get('/users', (req: Request, res: Response) => {
+    const user = req.body;
+    console.log(user);
+
+    res.json({
+        success: true,
+        message: "user is created successfully",
+        data: user
+    })
+})
+
+coursesRouter.post('/create-course', (req: Request, res: Response) => {
+    const course = req.body;
+    console.log(course);
+
+    res.json({
+        success: true,
+        message: "user is created successfully",
+        data: course
+    })
+})
+
+
+// the error
+app.get('/', logger, async (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+        res.send(something)
+
+    } catch (error) {
+
+        next(error)
+    }
 })
 
 
@@ -22,6 +60,26 @@ app.post('/', (req: Request, res: Response) => {
     res.send({
         message: "successfully receive data"
     })
+})
+
+
+
+// wrong route error show
+app.all("*", (req: Request, res: Response) => {
+    res.status(400).json({
+        success: false,
+        message: "Route is not found"
+    })
+})
+
+//global error
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+    if(error){
+        res.status(400).json({
+            success: false,
+            message: "Something went wrong"
+        })
+    }
 })
 
 export default app;
