763b4009752e7b8ec66f7ba89e23817e34f4e397
al-amin90 <ijesun30@gmail.com>
2025-12-16 12:15:26 +0600

    add routing

--- dist/app/app.js
+++ dist/app/app.js
@@ -7,8 +7,29 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.text());
+const userRouter = express_1.default.Router();
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/courses', userRouter);
+userRouter.get('/create-user', (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: 'Successfully',
+        data: user
+    });
+});
+userRouter.post('/create-course', (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: 'Successfully',
+        data: course
+    });
+});
 const logger = (req, res, next) => {
-    console.log(req.url, req.method, req.cookies, req.hostname);
+    // console.log(req.url, req.method, req.cookies, req.hostname);
     next();
 };
 app.get('/', logger, (req, res) => {
--- src/app/app.ts
+++ src/app/app.ts
@@ -3,8 +3,37 @@ const app = express()
 app.use(express.json())
 app.use(express.text())
 
+const userRouter = express.Router()
+
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/courses', userRouter)
+
+userRouter.get('/create-user', (req:Request, res: Response) => {
+    const user = req.body;
+    console.log(user);
+
+    res.json({
+        success: true,
+        message: 'Successfully',
+        data: user
+    })
+})
+userRouter.post('/create-course', (req:Request, res: Response) => {
+    const course = req.body;
+    console.log(course);
+
+    res.json({
+        success: true,
+        message: 'Successfully',
+        data: course
+    })
+})
+
+
+
+
 const logger = (req: Request, res: Response, next:NextFunction) => {
-    console.log(req.url, req.method, req.cookies, req.hostname);
+    // console.log(req.url, req.method, req.cookies, req.hostname);
 
     next()
 }
