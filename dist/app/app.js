4968df52677d768d495c48b3960cee00d80bb5fc
al-amin90 <ijesun30@gmail.com>
2025-12-16 11:52:50 +0600

    add middleware

--- dist/app/app.js
+++ dist/app/app.js
@@ -7,10 +7,16 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.text());
-app.get('/', (req, res) => {
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.cookies, req.hostname);
+    next();
+};
+app.get('/', logger, (req, res) => {
+    // console.log('params', req.params);
+    console.log('query', req.query);
     res.send('Hello DEvs./.!');
 });
-app.post('/', (req, res) => {
+app.post('/', logger, (req, res) => {
     console.log('body', req.body);
     res.json({ message: 'got the data' });
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,21 @@
-import  express, { Request, Response } from 'express'
+import  express, { NextFunction, Request, Response } from 'express'
 const app = express()
 app.use(express.json())
 app.use(express.text())
 
-app.get('/', (req: Request, res: Response) => {
+const logger = (req: Request, res: Response, next:NextFunction) => {
+    console.log(req.url, req.method, req.cookies, req.hostname);
+
+    next()
+}
+
+app.get('/',logger, (req: Request, res: Response) => {
+    // console.log('params', req.params);
+        console.log('query', req.query);
   res.send('Hello DEvs./.!')
 })
 
-app.post('/', (req: Request, res: Response) => {
+app.post('/',logger, (req: Request, res: Response) => {
     console.log('body', req.body);
     res.json({message:'got the data'})
 })
