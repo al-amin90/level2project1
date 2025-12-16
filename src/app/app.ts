1dc15e74013c155f6dd1b852c8ef081fe356489e
al-amin90 <ijesun30@gmail.com>
2025-12-16 11:12:29 +0600

    i express

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,85 +1,8 @@
-import { error } from 'console'
-import express, { NextFunction, Request, Response } from 'express'
+import  express from 'express'
 const app = express()
 
-//parsers
-app.use(express.json())
-app.use(express.text())
-
-//middleware
-const logger = (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.url, req.method);
-    next()
-}
-
-// creating diffarent route useing express.Router 
-const userRouter = express.Router();
-const coursesRouter = express.Router();
-
-app.use('/api/v1', userRouter)
-app.use('/api/v1/courses', coursesRouter)
-
-userRouter.get('/users', (req: Request, res: Response) => {
-    const user = req.body;
-    console.log(user);
-
-    res.json({
-        success: true,
-        message: "user is created successfully",
-        data: user
-    })
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-coursesRouter.post('/create-course', (req: Request, res: Response) => {
-    const course = req.body;
-    console.log(course);
-
-    res.json({
-        success: true,
-        message: "user is created successfully",
-        data: course
-    })
-})
-
-
-// the error
-app.get('/', logger, async (req: Request, res: Response, next: NextFunction) => {
-
-    try {
-        res.send(something)
-
-    } catch (error) {
-
-        next(error)
-    }
-})
-
-
-app.post('/', (req: Request, res: Response) => {
-    console.log(req.body);
-    res.send({
-        message: "successfully receive data"
-    })
-})
-
-
-
-// wrong route error show
-app.all("*", (req: Request, res: Response) => {
-    res.status(400).json({
-        success: false,
-        message: "Route is not found"
-    })
-})
-
-//global error
-app.use((error: any, req: Request, res: Response, next: NextFunction) => {
-    if(error){
-        res.status(400).json({
-            success: false,
-            message: "Something went wrong"
-        })
-    }
-})
-
-export default app;
+export default app
--- src/app/server.ts
+++ src/app/server.ts
@@ -1,15 +1,13 @@
 import { Server } from "http";
-import app from "./app"
+import app from "./app";
 
 const PORT = 5000;
 
-let server : Server;
+let server: Server;
 
-
-async function bootstrap() {
-    server = app.listen(PORT , () => {
-        console.log(`Example app listening on PORT ${PORT}`)
-    })
+function bootstrap() {
+  const server = app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+  });
 }
-
-bootstrap()
+bootstrap();
